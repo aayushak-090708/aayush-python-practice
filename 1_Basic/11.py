8a20455fe8b0112696a7036d3ca3170d2329e7db
aayushak-090708 <aayushak090708@gmail.com>
2026-06-26 21:07:45 +0530

    epetition until the no. 7 is typed

---
+++ 1_Basic/11.py
@@ -0,0 +1,8 @@
+#repetition until the no. 7 is typed
+a=False
+while a==False:
+    n=int(input("Enter a number: "))
+    if n==7:
+        a=True
+        print("You entered 7. Exiting the loop.")
+    
